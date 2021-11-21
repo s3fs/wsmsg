ba3443c04b5cd4c900094cf334924fef0ec8b5c7
s3fs <northfacebodybags@yahoo.com>
2021-11-22 00:22:43 +0500

    form+wssend

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,29 @@ const Message = (msg) => {
 
 const App = () => {
   const [messages, setMessages] = useState([])
+  const [text, setText] = useState('')
   const ws = useRef(null)
 
   useEffect(() => {
     ws.current = new WebSocket('wss://ws.qexsystems.ru')
+
+    ws.current.onopen = () => console.log('WSConn open')
+    ws.current.onclose = () => console.log('WSConn closed')
+
     ws.current.onmessage = msg => {
       setMessages(p => [...p, msg.data])
     }
+
+    return () => ws.current.close()
   }, [])
+
+  const handleFormSubmit = (ev) => {
+    ev.preventDefault()
+
+    console.log(`to be sent: ${text}`)
+    ws.current.send(text)
+    setText('')
+  }
 
   return(
     <div>
@@ -24,6 +39,14 @@ const App = () => {
       <ul>
         {messages.map(x => <li>{x}</li>)}
       </ul>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type='text'
+          value={text}
+          onChange={({ target }) => setText(target.value)}
+        />
+        <button type='submit'>Send</button>
+      </form>
     </div>
   )
 }
