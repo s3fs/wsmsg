e3ed225c2b60b61fcb5fdee28a1ab92f58995d85
s3fs <northfacebodybags@yahoo.com>
2021-11-22 15:10:06 +0500

    mvp

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+const userId = Math.floor(Math.random() * 1000000000)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App userId={userId} />
   </React.StrictMode>,
   document.getElementById('root')
 )
